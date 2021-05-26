f3ec6cdd74ec3575114a4ca18b912ce8eb31a8dd
lbabyuk <83418105+lbabyuk@users.noreply.github.com>
2021-05-26 12:17:26 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,1 @@
-const hour = document.getElementById('hours');
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const progress = document.getElementById('progress');
-function showCurrentTime() {
-    let date = new date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.hetSeconds();
-    hour.textContent = hr;
-    minute.textContent = min;
-    second.textContent = sec;
-    progress.style.width = (sec / 60) * 100 + '%'
-}
-setInterval(showCurrentTime, 1000)
+
