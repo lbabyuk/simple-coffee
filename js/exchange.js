c042c696c19fd5e78212a5f701082469679117aa
lbabyuk <lenababyuk@gmail.com>
2021-05-27 22:39:19 +0300

    exchange js

---
+++ js/exchange.js
@@ -0,0 +1,39 @@
+const currencyEl_one = document.querySelector("#currency-one");
+const amountEl_one = document.querySelectord("#amount-one");
+const currencyEl_two = document.querySelector("#currency-two");
+const amountEl_two = document.querySelector("#amount-two");
+
+const rateEl = document.querySelector("#rate");
+const swap = document.querySelector("#swap");
+
+// Fetch exchange rates and update the DOM
+function caclulate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+     
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  caclulate();
+});
+
+caclulate();
